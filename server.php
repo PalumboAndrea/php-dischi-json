6c7100d3c408d11981b5300aea243782cfcb12b3
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-30 19:23:17 +0100

    fix server.php

--- server.php
+++ server.php
@@ -4,6 +4,8 @@ $jsonData = file_get_contents(__DIR__ . '/db/dischi.json');
 
 $decodedJson = json_decode($jsonData, true);
 
+$discsList = [];
+
 if ($decodedJson != null) {
     $discsList = $decodedJson;
 }
